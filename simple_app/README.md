b4a95041765d6dc16288a8a4aefdd22aea1c3650
yijun <yijun@ucare.io>
2021-07-20 15:44:31 +0800

    finish up the simple app for celery worker to call..

---
+++ simple_app/README.md
@@ -0,0 +1,8 @@
+### Dockerize
+
+    docker build -t your_username/app_name:cool_version .
+
+
+### Run
+
+    docker run -d -p=8000:8000 your_username/app_name:cool_version
--- simple_app/app.py
+++ simple_app/app.py
@@ -5,8 +5,11 @@ import random
 
 app = Flask(__name__)
 
-@app.route("/elPsyCongroo", methods=['POST'])
+
+@app.route("/elPsyCongaroo", methods=['POST'])
 def lyra():
     wordline = request.args.get("worldline")
+    if wordline is None:
+        return "error...please add worldline as an integer in query parameter..."
     time.sleep(1)
     return {"wordline": int(wordline), "result": random.randint(0, 100)}
